5e7d27e742d56ea9a1c801bb91f8528fe05d8aa3
kjy1111 <ekfto7@nate.com>
2018-06-26 14:49:15 +0900

    Initial commit

---
+++ myhttpserver1.py
@@ -0,0 +1,19 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+port = 9999
+
+
+class MyHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write('<h1>Hello Woed!</h1>'.encode('utf-8'))
+
+
+# 서버 구동
+httpd = HTTPServer(('', 9999), MyHTTPRequestHandler)
+print('HTTP Server Starts.....')
+httpd.serve_forever()
